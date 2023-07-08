cfd7032656375e5d00d81138ffca9fc41fc6a79c
arkon <eugcheung94@gmail.com>
2023-07-08 16:26:05 -0400

    Migrate to Node.js

---
+++ index.mjs
@@ -0,0 +1,38 @@
+import * as cheerio from 'cheerio';
+import * as fs from 'fs';
+
+const request = await fetch('https://www.useragents.me');
+const document = await request.text();
+
+const $ = cheerio.load(document);
+
+const desktopRegex = new RegExp('^Mozilla\\/5\\.0 \\((Windows|Linux|Macintosh).*$');
+const mobileRegex = new RegExp('^Mozilla\\/5\\.0 \\((iPhone|iPad|Android).*$');
+
+const desktopUserAgents = [];
+const mobileUserAgents = [];
+
+$('.ua-textarea').map((_index, el) => {
+    const userAgent = $(el).text().trim();
+    if (!userAgent.startsWith('Mozilla/5.0 (')) {
+        return;
+    }
+
+    if (desktopRegex.test(userAgent)) {
+        desktopUserAgents.push(userAgent);
+    } else if (mobileRegex.test(userAgent)) {
+        mobileUserAgents.push(userAgent)
+    }
+});
+
+const userAgents = {
+    desktop: desktopUserAgents.sort(),
+    mobile: mobileUserAgents.sort(),
+};
+
+const buildDir = './build';
+if (!fs.existsSync(buildDir)) {
+    fs.mkdirSync(buildDir);
+}
+fs.writeFileSync(`${buildDir}/user-agents.json`, JSON.stringify(userAgents, null, 2));
+fs.writeFileSync(`${buildDir}/user-agents.min.json`, JSON.stringify(userAgents));
